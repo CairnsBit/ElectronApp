3405adc2fe18f17555eda9613dbabe3ed268eeac
CairnsBit <mnowicki586@outlook.com>
2024-03-15 12:13:25 +0100

    IPC

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-const { app, BrowserWindow } = require('electron')
+//Might skip the first lines because of the debugger
+
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+        preload: path.join(__dirname, 'Scripts/preload.js')
+    }
   })
 
   win.loadFile('Views/index.html')
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')  
   createWindow()
+
+  //Open a window if none are open (macOS)
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+//Close the app when all windows closed (Windows & Linux)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin'){
+        app.quit()
+        console.log('app closed')
+    } 
 })
